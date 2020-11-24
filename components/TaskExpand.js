e9fdde36571f11c34941d873cea7c60b3d1f252f
imnewt <maitantruc1999@gmail.com>
2020-11-24 23:58:03 +0700

    add overlay for task item

--- components/TaskExpand.js
+++ components/TaskExpand.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, Dimensions } from "react-native";
 import ProgressCircle from 'react-native-progress-circle'
 import EStyleSheet from 'react-native-extended-stylesheet';
-import { Button, Divider} from 'react-native-elements';
+import { Button, Divider, Overlay } from 'react-native-elements';
 
 export const LIST_ITEM_HEIGHT = Dimensions.get("window").width / 100 * 61.5;
+
+const TaskExpand = ({ description, progress }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleOverlay = () => {
+      // navigation.navigate("Main")
+    setVisible(false);
+  }
+  return (
+    <View style={styles.container}>
+      <Divider style={styles.divider}/>
+      <View style={styles.content}>
+        <Text style={styles.title}>Description</Text>
+        <Text style={styles.description} numberOfLines={1}>{description}</Text>
+        <View style={styles.progress}>
+          <ProgressCircle
+            percent={progress}
+            radius={40}
+            borderWidth={8}
+            color="#2ea7e0"
+            shadowColor="#C2C1BF"
+            bgColor="#fff"
+          >
+            <Text style={{ fontSize: 18 }}>{progress}%</Text>
+          </ProgressCircle>
+        </View>
+        <Button
+            title="details"
+            titleStyle={styles.buttonText}
+            buttonStyle={styles.button}
+            onPress={() => setVisible(true)}
+        />
+      </View>
+      <Overlay 
+        isVisible={visible}
+        onBackdropPress={toggleOverlay}
+        overlayStyle={{ borderRadius: 10, marginHorizontal: 20 }}
+      >
+        <View style={styles.overlay}>
+          <Text style={styles.title}>Description</Text>
+          <Text style={styles.description}>{description}</Text>
+        </View>
+      </Overlay>
+    </View>
+  )
+}
 
 const styles = EStyleSheet.create({
   container: {
@@ -49,37 +95,20 @@ const styles = EStyleSheet.create({
     color: "#fff",
     fontWeight: "bold",
     textTransform: "uppercase"
+  },
+  overlay: {
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    marginHorizontal: "3rem",
+    // paddingVertical: "8rem",
+    // paddingHorizontal: "15rem",
+    // borderRadius: 120
+  },
+  modalText: {
+      paddingTop: "3rem",
+      fontSize: "5rem",
+      fontWeight: "900"
   }
-});
-
-const TaskExpand = ({ description, progress }) => {
-  return (
-    <View style={styles.container}>
-      <Divider style={styles.divider}/>
-      <View style={styles.content}>
-        <Text style={styles.title}>Description</Text>
-        <Text style={styles.description} numberOfLines={1}>{description}</Text>
-        <View style={styles.progress}>
-          <ProgressCircle
-            percent={progress}
-            radius={40}
-            borderWidth={8}
-            color="#2ea7e0"
-            shadowColor="#C2C1BF"
-            bgColor="#fff"
-          >
-            <Text style={{ fontSize: 18 }}>{progress}%</Text>
-          </ProgressCircle>
-        </View>
-        <Button
-            title="details"
-            titleStyle={styles.buttonText}
-            buttonStyle={styles.button}
-            // onPress={handleLogin}
-        />
-      </View>
-    </View>
-  )
-}
+})
 
 export default TaskExpand;
